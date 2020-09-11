111064902d59622177e2a29e7399a2183c8b1975
AyushRanjan15 <ayushranjan15@gmail.com>
2020-09-11 12:37:08 +1000

    done

--- generator.py
+++ generator.py
@@ -1,19 +1,20 @@
+# This file lodes all file and keep only relavent one
+# creates only one df out of all data
+
 import os
 import pandas as pd
-import numpy as np
 
-# method to join df
-
-
+# columns to keep from hospital bed data
 joinedcolumns = ['X', 'Y', 'HOSPITAL_NAME', 'HOSPITAL_TYPE', 'HQ_CITY', 'HQ_STATE', 'HQ_ZIP_CODE',
                  'COUNTY_NAME', 'STATE_FIPS', 'CNTY_FIPS', 'FIPS', 'NUM_LICENSED_BEDS', 'NUM_STAFFED_BEDS',
                  'NUM_ICU_BEDS', 'ADULT_ICU_BEDS', 'PEDI_ICU_BEDS', 'BED_UTILIZATION',
                  'Potential_Increase_In_Bed_Capac',
                  'AVG_VENTILATOR_USAGE']
 
+# empty df
 data = pd.DataFrame(columns=joinedcolumns)
 
-
+# method to combine df
 def datacombiner(df, data):
     if data.shape[0] == 0:
         data = df
@@ -21,7 +22,7 @@ def datacombiner(df, data):
         data = pd.concat([data, df])
     return data
 
-
+# reading all files and preparing one df for hospital bed data
 data_folder = "/Users/ayushranjan/Documents/MY_Folder/csds/data"
 for months in os.listdir(data_folder):
     if months != ".DS_Store":
